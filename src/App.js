5675a8a7b906791fda0af94734af71505a5338a0
kkm980 <kantkrishna751@gmail.com>
2021-11-03 13:00:27 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+import axios from "axios";
+import {key} from "./Apikey";
 import Navbar from './components/Navbar';
+import NewsBox from './newsSee/NewsBox';
+
 
 function App() {
+  const [catg,setCatg] = useState("general");
+
+  const ourNews=async()=>{
+    try{
+      const newsDetails=await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`);
+       console.log(newsDetails);
+       console.log("hi");
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div className="App">
-        <Navbar/>
+        <Navbar setCatg={setCatg}/>
+        <NewsBox catg={catg}/>
     </div>
   );
 }
---
+++ src/newsSee/NewsBox.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material'
+import React from 'react'
+
+function NewsBox() {
+    return (
+        <div>
+            <Container maxWidth="md"></Container>
+        </div>
+    )
+}
+
+export default NewsBox;
